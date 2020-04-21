d6340e611e521fa408fd12f4a3d502c2f9dda3c6
kaikeUmobi <kaike@umobi.com.br>
2020-04-21 20:49:19 -0300

    migrations aula 50

--- database/migrations/2020_04_21_224827_create_products.php
+++ database/migrations/2020_04_21_224827_create_products.php
@@ -4,6 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * migrations são modelos de tabelas
+ * metodo up(): criar tabela, metodo down(): deletar tabela
+ * comandos:
+ * 1 - criar migration - php artisan make:migration
+ * 2 - desfazer ultima migration - php artisan migrate:rollback
+ * 3 - realizar migração
+ *      migrações são rodadas e salvas no banco na tabela migrations
+ *      assim o comando para fazer migrações somente cria tabelas que ainda não
+ *      existem no banco
+ *      php artisan migrate
+ * 4 - criar um unico campo em uma tabela existente
+ *      php artisan make:migration add_brand_to_products --table=products
+*/
+
 class CreateProducts extends Migration
 {
     /**
@@ -15,6 +30,10 @@ class CreateProducts extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('stock');
+            $table->double('price');
             $table->timestamps();
         });
     }
